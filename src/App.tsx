07e4158d778894093c00ae955c8df7fb71a44603
Marcos <marmbmti@gmail.com>
2024-07-28 19:15:42 -0300

    feat: configuring project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Teste } from "@/components/teste";
+
+export default function App() {
+  return (
+    <div>
+      <Teste />
+    </div>
+  );
+}
---
+++ src/components/teste/index.tsx
@@ -0,0 +1,7 @@
+export function Teste() {
+  return (
+    <div>
+      <h1>Teste</h1>
+    </div>
+  );
+}
